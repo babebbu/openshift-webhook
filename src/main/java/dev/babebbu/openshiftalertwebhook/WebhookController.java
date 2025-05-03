da4fef5c97be0c8d2845580b1e943075da190b26
Babebbu <babebbu@gmail.com>
2025-05-03 14:19:55 +0700

    First Commit

--- src/main/java/dev/babebbu/openshiftalertwebhook/WebhookController.java
+++ src/main/java/dev/babebbu/openshiftalertwebhook/WebhookController.java
@@ -3,6 +3,7 @@ package dev.babebbu.openshiftalertwebhook;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookController {
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<?> webhook(@RequestBody Object request) {
         log.info("{}", request);
         return ResponseEntity.ok().build();
